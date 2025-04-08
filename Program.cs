9859694461b329ec415edeaa0517bef881497305
NkatekoTekkie <99898616+NkatekoTekkie@users.noreply.github.com>
2025-04-08 20:36:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Media;
+
+namespace Alarm_clock
+{
+
+    public partial class AlarmClock : Form
+        {
+            private Timer timer;
+            private DateTime alarmTime;
+            private bool alarmSet = false;
+
+            public AlarmClock()
+            {
+                InitializeComponent();
+
+                // Setup timer to tick every second
+                timer = new Timer();
+                timer.Interval = 1000;
+                timer.Tick += Timer_Tick;
+                timer.Start();
+            }
+
+            private void AlarmClock_Load(object sender, EventArgs e)
+            {
+                lblCurrentTime.Text = DateTime.Now.ToLongTimeString();
+            }
+
+            private void Timer_Tick(object sender, EventArgs e)
+            {
+                lblCurrentTime.Text = DateTime.Now.ToLongTimeString();
+
+                if (alarmSet && DateTime.Now.ToLongTimeString() == alarmTime.ToLongTimeString())
+                {
+                    timer.Stop();
+                    alarmSet = false;
+                    SystemSounds.Beep.Play();
+                    MessageBox.Show("⏰ Alarm ringing!", "Alarm", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+
+            private void btnSetAlarm_Click(object sender, EventArgs e)
+            {
+                if (DateTime.TryParse(txtAlarmTime.Text, out alarmTime))
+                {
+                    alarmSet = true;
+                    MessageBox.Show("✅ Alarm set for " + alarmTime.ToLongTimeString(), "Alarm Set", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    MessageBox.Show("❌ Please enter time in HH:mm:ss format", "Invalid Time", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+    }
+
+}
